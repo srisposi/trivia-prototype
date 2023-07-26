4cea4a5466d57ca3c2b5835b0c89f1f2eaa42d36
srisposi <sdrisposi@gmail.com>
2023-07-26 13:01:19 -0300

    prototype-trivias

---
+++ app/data.js
@@ -0,0 +1,35 @@
+export const quiz = {
+  totalQuestions: 5,
+  questions: [
+    {
+      id: 1,
+      question: "Cúantos años se cumplen de Disney?",
+      answers: ["20", "30", "70", "100"],
+      correctAnswer: "100",
+    },
+    {
+      id: 2,
+      question: "Cuál fue uno de los países que recibió a Radio en Vivo?",
+      answers: ["Ecuador", "Colombia", "Venezuela", "Argentina"],
+      correctAnswer: "Argentina",
+    },
+    {
+      id: 3,
+      question: "Último estreno en Disney+?",
+      answers: ["Mulán", "Intensamente", "Avatar", "Star Wars IX"],
+      correctAnswer: "Avatar",
+    },
+    {
+      id: 4,
+      question: "Dial de Radio Disney en Argentina",
+      answers: ["99.1", "100.0", "94.3", "98.3"],
+      correctAnswer: "94.3",
+    },
+    {
+      id: 5,
+      question: "Nombre del artista conocido como PZK",
+      answers: ["Thiago", "Camilo", "Tini", "Ricardo"],
+      correctAnswer: "Thiago",
+    },
+  ],
+};
